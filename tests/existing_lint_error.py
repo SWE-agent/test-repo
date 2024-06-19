8c7e5bc43f6d14e733c51f69f9f4e6c81636a522
Kilian Lieret <kilian.lieret@posteo.de>
2024-06-18 20:00:20 -0400

    Add test for existing lint error

---
+++ tests/existing_lint_error.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+"""This file is to test the behavior of the agent when there is an existing 
+lint error in the codebase.
+"""
+
+
+def f821():
+    notdefined
+
+
+def division(a: float, b: float) -> float
+    return a/b
+
+
+if __name__ == "__main__":
+    print(division(123, 15))
+
